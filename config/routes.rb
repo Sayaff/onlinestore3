5a0e24f4808cb818e90dcceef3f219dbe747e03e
Sayaff <kir3217@gmail.com>
2016-09-11 20:08:09 +0300

    Added cart logic tutorial part 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get 'cart/index'
 
+  get '/cart' => 'cart#index'
+  get '/cart/clear' => 'cart#clearCart'
+  get '/cart/:id' => 'cart#add'
   resources :products
   root 'page#home'
-  
+
   get 'page/home'
 
   get 'page/about'
