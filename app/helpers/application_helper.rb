aa3d7db79c9a2d27f6f545511ed6096b93d003ab
Sayaff <kir3217@gmail.com>
2017-03-30 22:32:24 +0300

    Error messages fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def errors_for(object)
+      if object.errors.any?
+          content_tag(:div, id: "error_explanation") do
+                  concat(content_tag(:div, class: "alert alert-danger") do
+                      concat "The action failed because of the following errors:"
+                  end)
+                  concat(content_tag(:ul) do
+                      object.errors.full_messages.each do |msg|
+                          concat content_tag(:li, msg)
+                  end
+              end)
+          end
+      end
+  end
 end
